15c39b91fe512c14f367a7e1b9381e02c6889e44
Raul Martín Lozano <raumarl2@upv.edu.es>
2022-05-12 13:29:44 +0200

    comentar errores

--- app/src/main/java/com/example/android/shelted/Fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/android/shelted/Fragments/RegisterFragment.kt
@@ -17,8 +17,8 @@ import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.FirebaseDatabase
+//import com.google.firebase.auth.ktx.auth
+//import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.example.android.shelted.Classes.User
 import com.google.firebase.firestore.FirebaseFirestore
