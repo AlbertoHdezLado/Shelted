910f965f77535ade6d425c66283db6f5ef53163d
Alberto Hernández Lado <albertohljob@gmail.com>
2022-05-26 16:16:55 +0200

    FavouritesFragment.kt

--- app/src/main/java/com/example/android/shelted/Activities/LoggedActivity.kt
+++ app/src/main/java/com/example/android/shelted/Activities/LoggedActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.shelted.Activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
@@ -8,7 +9,9 @@ import android.os.Bundle
 import android.view.Window
 import android.widget.Button
 import androidx.appcompat.app.ActionBar
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.android.shelted.Fragments.*
 import com.example.android.shelted.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -18,9 +21,10 @@ import kotlinx.android.synthetic.main.activity_logged.*
 class LoggedActivity : AppCompatActivity() {
     private val postListFragment = postListFragment()
     private val favouritesFragment = FavouritesFragment()
-    private val messagesFragment = messageListFragment()
+    private val messageListFragment = messageListFragment()
     private val profileFragment = ProfileFragment()
 
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val actionBar: ActionBar? = supportActionBar
@@ -33,6 +37,7 @@ class LoggedActivity : AppCompatActivity() {
         transaction.addToBackStack(null)
         transaction.commit()
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottom_navigation.background = null
         bottom_navigation.menu.getItem(2).isEnabled = false
@@ -41,6 +46,7 @@ class LoggedActivity : AppCompatActivity() {
         bottom_navigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.buttonMain -> {
+                    fab.show()
                     val fragmentManager = supportFragmentManager
                     val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,postListFragment)
@@ -48,6 +54,7 @@ class LoggedActivity : AppCompatActivity() {
                     transaction.commit()
                     true}
                 R.id.buttonFavourites -> {
+                    fab.hide()
                     val fragmentManager = supportFragmentManager
                     val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,favouritesFragment)
@@ -55,13 +62,15 @@ class LoggedActivity : AppCompatActivity() {
                     transaction.commit()
                     true}
                 R.id.buttonMessages -> {
+                    fab.hide()
                     val fragmentManager = supportFragmentManager
                     val transaction = fragmentManager.beginTransaction()
-                    transaction.replace(R.id.logged_activity,messagesFragment)
+                    transaction.replace(R.id.logged_activity,messageListFragment)
                     transaction.addToBackStack(null)
                     transaction.commit()
                     true}
                 R.id.buttonProfile -> {
+                    fab.hide()
                     val fragmentManager = supportFragmentManager
                     val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,profileFragment)
@@ -72,8 +81,7 @@ class LoggedActivity : AppCompatActivity() {
             }
         }
 
-        val add_post = findViewById<FloatingActionButton>(R.id.fab)
-        add_post.setOnClickListener {
+        fab.setOnClickListener {
             startActivity(Intent(this,PublishActivity::class.java ))
         }
 
