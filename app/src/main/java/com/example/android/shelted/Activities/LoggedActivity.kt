115432d7cf7bd26d7915eedcd21cacf13c23772f
Alberto Hernández Lado <albertohljob@gmail.com>
2022-05-26 18:49:25 +0200

    shelter status

--- app/src/main/java/com/example/android/shelted/Activities/LoggedActivity.kt
+++ app/src/main/java/com/example/android/shelted/Activities/LoggedActivity.kt
@@ -1,13 +1,17 @@
 package com.example.android.shelted.Activities
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import android.view.Window
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
@@ -16,6 +20,8 @@ import com.example.android.shelted.Fragments.*
 import com.example.android.shelted.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_logged.*
 
 class LoggedActivity : AppCompatActivity() {
@@ -37,6 +43,27 @@ class LoggedActivity : AppCompatActivity() {
         transaction.addToBackStack(null)
         transaction.commit()
 
+        var shelter = false
+
+        val auth = FirebaseAuth.getInstance()
+        val email = auth.currentUser?.email
+        val db = FirebaseFirestore.getInstance()
+        val usersRef = db.collection("users")
+        usersRef.document(email!!).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document.exists()) {
+                    shelter = document.getBoolean("shelter")!!
+                } else {
+                    Log.d(ContentValues.TAG, "The document doesn't exist.")
+                }
+            } else {
+                task.exception?.message?.let {
+                    Log.d(ContentValues.TAG, "Error in task")
+                }
+            }
+        }
+
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottom_navigation.background = null
@@ -44,35 +71,29 @@ class LoggedActivity : AppCompatActivity() {
 
 
         bottom_navigation.setOnItemSelectedListener { item ->
+            val fragmentManager = supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
             when (item.itemId) {
                 R.id.buttonMain -> {
                     fab.show()
-                    val fragmentManager = supportFragmentManager
-                    val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,postListFragment)
                     transaction.addToBackStack(null)
                     transaction.commit()
                     true}
                 R.id.buttonFavourites -> {
                     fab.show()
-                    val fragmentManager = supportFragmentManager
-                    val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,favouritesFragment)
                     transaction.addToBackStack(null)
                     transaction.commit()
                     true}
                 R.id.buttonMessages -> {
                     fab.hide()
-                    val fragmentManager = supportFragmentManager
-                    val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,messageListFragment)
                     transaction.addToBackStack(null)
                     transaction.commit()
                     true}
                 R.id.buttonProfile -> {
                     fab.show()
-                    val fragmentManager = supportFragmentManager
-                    val transaction = fragmentManager.beginTransaction()
                     transaction.replace(R.id.logged_activity,profileFragment)
                     transaction.addToBackStack(null)
                     transaction.commit()
@@ -82,7 +103,10 @@ class LoggedActivity : AppCompatActivity() {
         }
 
         fab.setOnClickListener {
-            startActivity(Intent(this,PublishActivity::class.java ))
+            if (shelter)
+                startActivity(Intent(this,PublishActivity::class.java ))
+            else
+                Toast.makeText(this, "You need to be a shelter to publish a post!", Toast.LENGTH_SHORT).show()
         }
 
         val states = arrayOf(
@@ -94,7 +118,6 @@ class LoggedActivity : AppCompatActivity() {
             Color.parseColor("#D3D3D3"))
 
         bottom_navigation.itemIconTintList = ColorStateList(states, colors)
-
     }
 
     override fun onBackPressed() {
